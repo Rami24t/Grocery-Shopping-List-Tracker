e11854ee9ce44865e3e45e32aff05e33f32c4f8d
Rami Al-Saadi <alsaadi.rami24t@gmail.com>
2023-06-21 20:36:13 +0200

    start styling the app using Tailwind CSS

--- client/src/App.js
+++ client/src/App.js
@@ -59,7 +59,7 @@ function App() {
       setItems(JSON.parse(localStorage.getItem("list" + list)));
     // else setItems([]);
     else setItems(defaultItems);
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [list]);
 
   const handleSave = (updatedItems) => {
@@ -67,7 +67,7 @@ function App() {
     localStorage.setItem("list" + list, JSON.stringify(updatedItems || items));
     console.log("save end");
   };
-  
+
   const handleDelete = (item) => {
     setItems((prevItems) => {
       const updatedItems = prevItems.filter((i) => i.id !== item.id);
@@ -135,9 +135,70 @@ function App() {
   }, [items]);
 
   return (
-    <div className="App">
+    <div className="App bg-white dark:bg-gray-900 dark:text-white">
       <header className="App-header">
-        <h1>Grocery List</h1>
+        <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
+          <div className="sm:flex sm:items-center sm:justify-between">
+            <div className="text-center sm:text-left">
+              <h1 className="text-2xl font-bold dark:text-white text-gray-900 sm:text-3xl">
+                Grocery List
+              </h1>
+              <p className="mt-1.5 text-sm text-gray-500">Let's do it! 🎉</p>
+            </div>
+
+            <div className="mt-4 flex flex-col gap-4 sm:mt-0 sm:flex-row sm:items-center">
+              <button
+                className="relative block rounded-lg bg-indigo-600 px-5 py-3 text-sm font-medium text-white transition hover:bg-indigo-700 focus:outline-none focus:ring"
+                // "inline-flex items-center justify-center gap-1.5 rounded-lg border text-gray-500 hover:text-gray-700"
+                type="button"
+              >
+                Add Items{" "}
+                <svg
+                  className="inline w-4 h-4 text-gray-800 dark:text-white"
+                  ariaHidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 18 20"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M12 9V4a3 3 0 0 0-6 0v5m9.92 10H2.08a1 1 0 0 1-1-1.077L2 6h14l.917 11.923A1 1 0 0 1 15.92 19Z"
+                  />
+                </svg>
+                {/* <svg
+                  className="inline-block w-4 h-4 text-gray-800 dark:text-white"
+                  ariaHidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 18 20"
+                >
+                  <path d="M17 5.923A1 1 0 0 0 16 5h-3V4a4 4 0 1 0-8 0v1H2a1 1 0 0 0-1 .923L.086 17.846A2 2 0 0 0 2.08 20h13.84a2 2 0 0 0 1.994-2.153L17 5.923ZM7 9a1 1 0 0 1-2 0V7h2v2Zm0-5a2 2 0 1 1 4 0v1H7V4Zm6 5a1 1 0 1 1-2 0V7h2v2Z" />
+                </svg>*/}{" "}
+                <svg
+                  className={` inline-block w-4 h-4 text-gray-800 dark:text-white`}
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 8 14"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="1"
+                    d="m1 13 5.7-5.326a.909.909 0 0 0 0-1.348L1 1"
+                  />
+                </svg>{" "}
+                <div className="absolute inline-flex items-center justify-center w-6 h-6 text-xs font-bold text-white bg-red-500 border-2 border-white rounded-full -top-2 -right-2 dark:border-gray-900">
+                  {items.length || 0}{" "}
+                </div>
+              </button>
+            </div>
+          </div>
+        </div>
       </header>
       <main>
         <aside>
@@ -180,12 +241,22 @@ function App() {
               disabled={false}
             />
           </h2>
-          <input
-            type="text"
-            name="new-item"
-            placeholder="New Item"
-            onBlur={handleAdd}
-          />
+          <div className="mb-6">
+            <label
+              htmlFor="new-item"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              New Item
+            </label>
+            <input
+              name="new-item"
+              placeholder="New Item"
+              onBlur={handleAdd}
+              type="text"
+              id="new-item"
+              className="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            />
+          </div>
           <button>+</button>
           <br />
           {items.length === 0 ? (
@@ -226,6 +297,7 @@ function App() {
           )}
         </section>
         <aside>
+          <img src="https://via.placeholder.com/150x350" alt="placeholder" />
           <img src="https://via.placeholder.com/150x350" alt="placeholder" />
         </aside>
       </main>
