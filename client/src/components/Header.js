5fc53e0ccda0a70e0b0f11ed0403a76f8731fe71
Rami <alsaadi.rami24@gmail.com>
2023-07-04 20:01:42 +0200

    styles - clean - add list number

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,11 +1,11 @@
-function Header({ items }) {
+function Header({ list, items }) {
   return (
     <header className="App-header z-50 sticky bg-inherit w-full">
       <div className="mx-auto max-w-screen-xl px-2 py-4 sm:px-3 sm:py-6 lg:px-8">
         <div className="sm:flex sm:items-center sm:justify-between">
           <div className="text-center sm:text-left">
             <h1 className="text-2xl font-bold dark:text-white text-gray-900 sm:text-3xl">
-              Grocery List
+              Grocery List 🛒 {list}
             </h1>
             <p className="mt-1.5 text-sm text-gray-500">Let's do it! 🎉</p>
           </div>
@@ -43,7 +43,7 @@ function Header({ items }) {
             <path d="M17 5.923A1 1 0 0 0 16 5h-3V4a4 4 0 1 0-8 0v1H2a1 1 0 0 0-1 .923L.086 17.846A2 2 0 0 0 2.08 20h13.84a2 2 0 0 0 1.994-2.153L17 5.923ZM7 9a1 1 0 0 1-2 0V7h2v2Zm0-5a2 2 0 1 1 4 0v1H7V4Zm6 5a1 1 0 1 1-2 0V7h2v2Z" />
           </svg>*/}{" "}
               <div className="absolute inline-flex items-center justify-center w-6 h-6 text-xs font-bold text-white bg-red-500 border-2 border-white rounded-full -top-2 -right-2 dark:border-gray-900">
-                {items.length || 0}{" "}
+                {items || 0}{" "}
               </div>
             </button>
           </div>
