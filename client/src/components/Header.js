6cd5ccf420b943b916a9e72a0a21389c96e4502d
Rami <alsaadi.rami24@gmail.com>
2023-07-04 03:30:13 +0200

    edit header button

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -12,11 +12,12 @@ function Header({ items }) {
 
           <div className="mt-4 flex flex-col gap-4 sm:mt-0 sm:flex-row sm:items-center">
             <button
+            disabled
               className="relative block rounded-lg bg-indigo-600 px-5 py-3 text-sm font-medium text-white transition hover:bg-indigo-700 focus:outline-none focus:ring"
               // "inline-flex items-center justify-center gap-1.5 rounded-lg border text-gray-500 hover:text-gray-700"
               type="button"
             >
-              Add Items{" "}
+              Items{"  "}
               <svg
                 className="inline w-4 h-4 text-gray-800 dark:text-white"
                 aria-hidden="true"
@@ -41,21 +42,6 @@ function Header({ items }) {
           >
             <path d="M17 5.923A1 1 0 0 0 16 5h-3V4a4 4 0 1 0-8 0v1H2a1 1 0 0 0-1 .923L.086 17.846A2 2 0 0 0 2.08 20h13.84a2 2 0 0 0 1.994-2.153L17 5.923ZM7 9a1 1 0 0 1-2 0V7h2v2Zm0-5a2 2 0 1 1 4 0v1H7V4Zm6 5a1 1 0 1 1-2 0V7h2v2Z" />
           </svg>*/}{" "}
-              <svg
-                className={` inline-block w-4 h-4 text-gray-800 dark:text-white`}
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 8 14"
-              >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="1"
-                  d="m1 13 5.7-5.326a.909.909 0 0 0 0-1.348L1 1"
-                />
-              </svg>{" "}
               <div className="absolute inline-flex items-center justify-center w-6 h-6 text-xs font-bold text-white bg-red-500 border-2 border-white rounded-full -top-2 -right-2 dark:border-gray-900">
                 {items.length || 0}{" "}
               </div>
