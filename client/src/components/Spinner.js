dc2525e8dceafbfbc89fdb89314411c0890d0982
Rami <alsaadi.rami24@gmail.com>
2023-07-05 14:31:54 +0200

    jsx

--- client/src/components/Spinner.js
+++ client/src/components/Spinner.js
@@ -5,7 +5,7 @@ const Spinner = ({size=8}) => {
     <div role="status">
       <svg
         aria-hidden="true"
-        class={`w-${size} h-${size} mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"`}
+        className={`w-${size} h-${size} mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"`}
         viewBox="0 0 100 101"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
@@ -19,7 +19,7 @@ const Spinner = ({size=8}) => {
           fill="currentFill"
         />
       </svg>
-      <span class="sr-only">Loading...</span>
+      <span className="sr-only">Loading...</span>
     </div>
   );
 };
