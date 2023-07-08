4b6030af3df39decec3bcc2b937d475af1a0e658
Rami <alsaadi.rami24@gmail.com>
2023-07-08 15:55:05 +0200

    add mt

--- client/src/components/footer/CopyrightDisclaimer.js
+++ client/src/components/footer/CopyrightDisclaimer.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function CopyrightDisclaimer({ year = "2023", author = "Rami Al-Saadi™", link = "https://www.linkedin.com/in/rami-al-saadi-16a14223a/" }) {
-  return <span className="copyright-disclaimer text-sm text-gray-500 sm:text-center dark:text-gray-400">
+  return <span className="copyright-disclaimer mt-1 text-sm text-gray-500 sm:text-center dark:text-gray-400">
     © {year}{" "}
     <a
       href="https://www.linkedin.com/in/rami-al-saadi-16a14223a/"
