e90c8cc2365118b308a8f194bdd81cf033e0a2e3
Rami <alsaadi.rami24@gmail.com>
2023-07-08 17:20:14 +0200

    debug styles

--- client/src/App.js
+++ client/src/App.js
@@ -167,7 +167,7 @@ function App() {
       }`}
     >
       <Header list={list} items={items.length} />
-      <div className="bg-inherit flex justify-around flex-wrap gap-10 relative ">
+      <div className="overflow-x-hidden bg-inherit flex justify-around flex-wrap gap-10 relative">
         <aside className="aside-left bg-inherit">
           <div
             className={`z-50 fixed top-0 ${
