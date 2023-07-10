4686c905c0eb05aca3ec609419e60f300a1b8f62
Rami <alsaadi.rami24@gmail.com>
2023-07-10 02:35:30 +0200

    implement dark theme

--- client/src/components/main-div/AddItems.js
+++ client/src/components/main-div/AddItems.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { BsPlusSquare, BsFillPlusSquareFill } from "react-icons/bs";
 
-function AddItems({ handleAdd }) {
+function AddItems({ handleAdd, dark }) {
   return (
-    <section className="w-92 mx-auto mt-3 mb-5 pt-3 pb-5 add-item dark:bg-gray-900 dark:border-gray-700 dark:text-white bg-gray-50 border border-gray-300 rounded-lg shadow-sm">
+    <section className={`"w-92 mx-auto mt-3 mb-5 pt-3 pb-5 add-item border rounded-lg ${dark?'bg-gray-900 border-gray-700 text-white':'bg-gray-50 border-gray-300'} shadow-sm `}>
       <label
         htmlFor="new-item"
-        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        className={`block mb-2 text-sm font-medium ${!dark?'text-gray-900':'text-white'} `}
       >
         <h3 className="text-center text-lg">Add Items</h3>
       </label>
@@ -17,11 +17,11 @@ function AddItems({ handleAdd }) {
           onBlur={handleAdd}
           type="text"
           id="new-item"
-          className="inline-block p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          className={` ${dark?'placeholder-gray-400 text-white border-gray-600 bg-gray-700 ':'text-gray-900 border-gray-300 bg-gray-50 '} inline-block p-4 border rounded-lg sm:text-md focus:ring-blue-500 focus:border-blue-500  `}
           onKeyDown={(e) => e.key === "Enter" && handleAdd(e)}
         />
         <button
-          className="inline-block p-3 text-center text-gray-500 hover:text-gray-900 dark:hover:text-white"
+          className={`${dark?'hover:text-white':'hover:text-gray-900'} inline-block p-3 text-center text-gray-500  `}
           onClick={(e) => {
             e.preventDefault();
             e.stopPropagation();
@@ -29,7 +29,7 @@ function AddItems({ handleAdd }) {
           }}
         >
           {(
-            <BsPlusSquare className="w-8 h-8 mx-auto text-gray-900 active:outline-none rounded-full border border-gray-200 hover:text-blue-700 active:z-10 active:ring-2 active:ring-gray-200 dark:active:ring-gray-700 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white" />
+            <BsPlusSquare className={`${dark?' active:ring-gray-700 text-gray-400 border-gray-600 hover:text-white ':'text-gray-900 border-gray-200 active:ring-gray-200 hover:text-blue-700 '} w-8 h-8 mx-auto  active:outline-none rounded-full border active:z-10 active:ring-2  `} />
           ) || <BsFillPlusSquareFill /> ||
             "+"}
         </button>
