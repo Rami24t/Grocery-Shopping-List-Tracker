37971f788edc7edbfb022ee09a33cb7e715a54ab
Rami <alsaadi.rami24@gmail.com>
2023-07-10 02:36:14 +0200

    implement dark theme

--- client/src/components/main-div/AddItemsButton.js
+++ client/src/components/main-div/AddItemsButton.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function AddItemsButton({ showAddItem, setShowAddItem, listIsEmpty }) {
+function AddItemsButton({ showAddItem, setShowAddItem, listIsEmpty, dark }) {
   // const toggleAddBtn = useRef(null);
   listIsEmpty && setShowAddItem(true);
   return (
@@ -8,7 +8,7 @@ function AddItemsButton({ showAddItem, setShowAddItem, listIsEmpty }) {
       // ref={toggleAddBtn}
       disabled={listIsEmpty}
       onClick={() => setShowAddItem(!showAddItem)}
-      className={`transition-all inline-flex items-center justify-between gap-1.5 w-10 relative rounded-lg
+      className={`font-extrabold transition-all inline-flex items-center justify-between gap-1.5 w-10 relative rounded-lg
       ${
         !showAddItem
           ? "bg-indigo-700  hover:bg-indigo-600"
@@ -20,13 +20,13 @@ function AddItemsButton({ showAddItem, setShowAddItem, listIsEmpty }) {
     >
       {showAddItem ? "Add " : "Add "}
       <div
-        className={`transition-transform flex text-center items-center justify-center w-3 h-3 text-gray-800 dark:text-white ${
-          showAddItem ? "rotate-90" : "rotate-0"
+        className={`font-extrabold transition-transform flex text-center items-center justify-center w-3 h-3 ${!dark?'text-gray-800':'text-white'} ${
+          showAddItem ? "rotate-90" : "rotate-0 transform -translate-y-0.5"
         }`}
       >
         &gt;
       </div>
-      {/* <div className="absolute inline-flex items-center justify-center w-6 h-6 text-xs font-bold text-white bg-red-500 border-2 border-white rounded-full -top-2 -right-2 dark:border-gray-900">
+      {/* <div className="absolute inline-flex items-center justify-center w-6 h-6 text-xs font-bold text-white bg-red-500 border-2 border-white rounded-full -top-2 -right-2 border-gray-900">
               {items.length || 0}{" "}
             </div> */}
     </button>
