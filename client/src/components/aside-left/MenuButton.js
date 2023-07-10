82d6c7ac402ad860f13e534284b1dcd29c5b5e45
Rami <alsaadi.rami24@gmail.com>
2023-07-10 02:29:19 +0200

    edit bb styles

--- client/src/components/aside-left/MenuButton.js
+++ client/src/components/aside-left/MenuButton.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./MenuButton.css";
 
-const MenuButton = ({ showSideNav, setShowSideNav }) => {
+const MenuButton = ({ showSideNav, setShowSideNav,dark }) => {
   return (
     <div
       onClick={() => setShowSideNav((prev) => !prev)}
-     className={`text-white rounded-bl-2xl bg-gray-900 max-h-[60px] overflow-hidden cursor-pointer menu-btn z-50 ${showSideNav ? "open " : ""}`}>
+     className={`rounded-bl-2xl bg-gray-900 max-h-[60px] overflow-hidden cursor-pointer menu-btn z-50 ${showSideNav ? "open " : ""}`}>
       <div className={`ml-3 icon-left`}></div>
       <div className={`ml-3 icon-right`}></div>
     </div>
