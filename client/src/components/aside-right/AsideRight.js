581a456c882f2dd7aaf4984d22975c15d599431b
Rami <alsaadi.rami24@gmail.com>
2023-07-10 02:30:59 +0200

    update deco images # and style

--- client/src/components/aside-right/AsideRight.js
+++ client/src/components/aside-right/AsideRight.js
@@ -24,7 +24,11 @@ function AsideRight() {
       "/assets/deco-imgs/deco3.jpg",
       "/assets/deco-imgs/deco11.jpg",
       "/assets/deco-imgs/deco2.jpg",
-      "/assets/deco-imgs/deco4.jpg",
+      "/assets/deco-imgs/deco9.jpg",
+      "/assets/deco-imgs/deco10.jpg",
+      "/assets/deco-imgs/deco5.jpg",
+      "/assets/deco-imgs/deco1.jpg",
+      "/assets/deco-imgs/deco0.jpg",
     ];
     if (extraImagesNeeded > 0)
       for (let i = 0; i < extraImagesNeeded; i++)
@@ -38,26 +42,33 @@ function AsideRight() {
   }, [height]);
 
   return (
+
     <aside
       ref={ref}
       // onResize={() => setHeight(ref.current.clientHeight)}
       id="aside-right"
       className="opacity-90 p-2 lg:pt-[220px] lg:max-w-[25%] text-center flex flex-wrap gap-2 lg:gap-4 justify-center items-center relative z-0"
     >
+
+
       <AsideImage src="/assets/deco-imgs/deco8.jpg" />
+
       {extraImages.slice(0, 1).map((src, idx) => (
         <AsideImage className={"lg:hidden"} key={idx} src={src} />
       ))}
+
       {extraImages.slice(1, 3).map((src, idx) => (
         <AsideImage className={"sm:hidden"} key={idx} src={src} />
       ))}
 
       {extraImages.map(
         (src, idx) =>
-          height > (idx + 1 + 1) * 390 + 300 && (
+          (height > (idx + 1 + 1) * 390 + 300) && (
             <AsideImage key={idx} src={src} />
           )
       )}
+
+
     </aside>
   );
 }
