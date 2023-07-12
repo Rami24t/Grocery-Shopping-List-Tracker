673fb8f3a8e79f6cf7ecf405f5ba996fe26a6362
Rami <alsaadi.rami24@gmail.com>
2023-07-12 17:53:05 +0200

    refractor code - better readability & performance

--- client/src/components/main-div/FilterSection.js
+++ client/src/components/main-div/FilterSection.js
@@ -1,67 +1,64 @@
 import React from "react";
 import { BsFunnel, BsFunnelFill } from "react-icons/bs";
+import { SvgSearch } from "./SvgSearch";
 
 function FilterSection({ filter, setFilter, handleChangeFilter, items, dark }) {
-  
+  if (items.length <= 1) return null;
+
   function onEnter(e) {
-    if(e.key === 'Enter')
-      e.target.blur()
+    if (e.key === "Enter") e.target.blur();
   }
   function clearFilter() {
     setFilter("");
   }
 
+  function FilterInput() {
+    return (
+      <input
+        type="text"
+        id="filter"
+        name="filter"
+        value={filter}
+        onChange={handleChangeFilter}
+        onKeyDown={onEnter}
+        className={`w-64 border text-sm rounded-lg focus:ring-blue-500  focus:border-blue-500 block pl-10 p-2.5 ${
+          !dark
+            ? "bg-gray-50 border-gray-300 text-gray-900"
+            : "placeholder-gray-400 bg-gray-700 bg-opacity-70 border-gray-600 text-white"
+        }`}
+        placeholder={`Search ${items.length} Items`}
+      />
+    );
+  }
+  function ClearFilterBtn() {
+    return (
+      <button
+        onClick={clearFilter}
+        className="font-bold text-xl absolute inset-y-0 left-80 flex items-center pr-3"
+      >
+        {filter.length > 0 ? (
+          <>
+            <BsFunnelFill className="inline text-gray-100" />{" "}
+            <div className="pointer-events-none inline w-[3px] h-8 rounded-full rotate-45 -translate-x-3 -translate-y-[3px] bg-red-400 opacity-75 "></div>
+          </>
+        ) : (
+          <BsFunnel className="inline relative "></BsFunnel>
+        )}
+      </button>
+    );
+  }
   return (
-    items.length > 1 && (
-      <section className="me-auto -ms-5 sm:mx-auto relative max-w-max my-1 text-center flex items-center ">
-        <label htmlFor="filter" className="sr-only">
-          Filter Items
-        </label>
-        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-          <svg
-            aria-hidden="true"
-            className={`w-5 h-5 ${dark ? " text-gray-400 " : " text-gray-500"}`}
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fillRule="evenodd"
-              d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-              clipRule="evenodd"
-            ></path>
-          </svg>
-        </div>
-        <input
-          type="text"
-          id="filter"
-          name="filter"
-          value={filter}
-          onChange={handleChangeFilter}
-          onKeyDown={onEnter}
-          className={`w-64 border text-sm rounded-lg focus:ring-blue-500  focus:border-blue-500 block pl-10 p-2.5 ${
-            !dark
-              ? "bg-gray-50 border-gray-300 text-gray-900"
-              : "placeholder-gray-400 bg-gray-700 bg-opacity-70 border-gray-600 text-white"
-          }`}
-          placeholder={`Search ${items.length} Items`}
-        />
-        <button
-          onClick={clearFilter}
-          className="font-bold text-xl absolute inset-y-0 left-80 flex items-center pr-3"
-        >
-          {filter.length > 0 ? (
-            <>
-              <BsFunnelFill className="inline text-gray-100" />{" "}
-              <div className="pointer-events-none inline w-[3px] h-8 rounded-full rotate-45 -translate-x-3 -translate-y-[3px] bg-red-400 opacity-75 "></div>
-            </>
-          ) : (
-            <BsFunnel className="inline relative "></BsFunnel>
-          )}
-        </button>
-      </section>
-    )
+    <section className="me-auto -ms-5 sm:mx-auto relative max-w-max my-1 text-center flex items-center">
+      <label htmlFor="filter" className="sr-only">
+        Filter Items
+      </label>
+      <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+        <SvgSearch dark={dark} />
+      </div>
+      <FilterInput />
+      <ClearFilterBtn />
+    </section>
   );
 }
 
-export default FilterSection;
+export default React.memo(FilterSection);
