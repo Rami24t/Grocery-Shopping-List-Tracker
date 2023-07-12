9f9c2db91ed01412a422a0966894f89690388d6a
Rami <alsaadi.rami24@gmail.com>
2023-07-12 03:13:33 +0200

    develop Add Items code and styles

--- client/src/components/main-div/AddItems.js
+++ client/src/components/main-div/AddItems.js
@@ -1,9 +1,22 @@
 import React from "react";
-import { BsPlusSquare, BsFillPlusSquareFill } from "react-icons/bs";
+import { BsPlusCircle } from "react-icons/bs";
 
 function AddItems({ handleAdd, dark }) {
+
+  function handleClick() {
+    return (e) => {
+      e.preventDefault();
+      e.stopPropagation();
+      e.currentTarget.previousSibling.focus();
+    };
+  }
+  
+  function onEnter() {
+    return (e) => e.key === "Enter" && handleAdd(e);
+  }
+
   return (
-    <section className={`"w-92 mx-auto mt-3 mb-5 pt-3 pb-5 add-item border rounded-lg ${dark?'bg-gray-900 border-gray-700 text-white':'bg-gray-50 border-gray-300'} shadow-sm `}>
+    <section className={`w-80 mx-auto mt-4 mb-5 pl-3 pt-3 pb-5 add-item border rounded-lg ${dark?'bg-gray-900 border-gray-700 text-white':'bg-gray-50 border-gray-300'} shadow-sm `}>
       <label
         htmlFor="new-item"
         className={`block mb-2 text-sm font-medium ${!dark?'text-gray-900':'text-white'} `}
@@ -18,20 +31,17 @@ function AddItems({ handleAdd, dark }) {
           type="text"
           id="new-item"
           className={` ${dark?'placeholder-gray-400 text-white border-gray-600 bg-gray-700 ':'text-gray-900 border-gray-300 bg-gray-50 '} inline-block p-4 border rounded-lg sm:text-md focus:ring-blue-500 focus:border-blue-500  `}
-          onKeyDown={(e) => e.key === "Enter" && handleAdd(e)}
+          onKeyDown={onEnter()}
         />
         <button
+        title="Add Item"
           className={`${dark?'hover:text-white':'hover:text-gray-900'} inline-block p-3 text-center text-gray-500  `}
-          onClick={(e) => {
-            e.preventDefault();
-            e.stopPropagation();
-            e.currentTarget.previousSibling.focus();
-          }}
+          onClick={handleClick()}
         >
           {(
-            <BsPlusSquare className={`${dark?' active:ring-gray-700 text-gray-400 border-gray-600 hover:text-white ':'text-gray-900 border-gray-200 active:ring-gray-200 hover:text-blue-700 '} w-8 h-8 mx-auto  active:outline-none rounded-full border active:z-10 active:ring-2  `} />
-          ) || <BsFillPlusSquareFill /> ||
-            "+"}
+            <BsPlusCircle className={`${dark?' active:ring-gray-700 text-gray-200 border-gray-600 hover:text-gray-100 opacity-70 ':'text-gray-900 border-gray-200 active:ring-gray-200 hover:text-blue-700 '} w-8 h-8 mx-auto  active:outline-none rounded-full border active:z-10 active:ring-2  
+            `} />
+          )}
         </button>
       </div>
     </section>
