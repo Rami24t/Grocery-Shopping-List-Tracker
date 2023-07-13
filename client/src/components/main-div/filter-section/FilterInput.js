5f0b549dc594a7c3ef5ead745b5d3fdf5b15f503
Rami <alsaadi.rami24@gmail.com>
2023-07-14 00:22:45 +0200

    edit styles - responsivity/width/layout

--- client/src/components/main-div/filter-section/FilterInput.js
+++ client/src/components/main-div/filter-section/FilterInput.js
@@ -13,7 +13,7 @@ function FilterInput({filter, handleChangeFilter, items, dark}) {
       value={filter}
       onChange={handleChangeFilter}
       onKeyDown={onEnter}
-      className={`w-64 border text-sm rounded-lg focus:ring-blue-500  focus:border-blue-500 block pl-10 p-2.5 ${!dark
+      className={`ms-0.5 w-56 md:w-64 lg:w-80 border text-sm rounded-lg focus:ring-blue-500  focus:border-blue-500 block pl-7 p-2.5 ${!dark
           ? "bg-gray-50 border-gray-300 text-gray-900"
           : "placeholder-gray-400 bg-gray-700 bg-opacity-70 border-gray-600 text-white"}`}
       placeholder={`Search ${items} Items`} />
--- client/src/components/main-div/filter-section/FilterSection.js
+++ client/src/components/main-div/filter-section/FilterSection.js
@@ -7,11 +7,11 @@ function FilterSection({ filter, setFilter, handleChangeFilter, items, dark }) {
   if (items <= 1) return null;
 
   return (
-    <section className="me-auto w-full -ms-1 sm:mx-auto relative max-w-max my-1 text-center flex items-center ">
+    <section className="me-auto w-full sm:mx-auto relative max-w-max my-1 text-center flex items-center ">
       <label htmlFor="filter" className="sr-only">
         Filter Items
       </label>
-      <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+      <div className="absolute left-1.5 flex items-center pointer-events-none">
         <SvgSearch dark={dark} />
       </div>
       <FilterInput filter={filter} handleChangeFilter={handleChangeFilter} items={items} dark={dark} />
