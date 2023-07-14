091360658b8086cb4cc45d2ea7b3e5c09eb01d49
Rami <alsaadi.rami24@gmail.com>
2023-07-14 05:38:14 +0200

    debug fix refractor

--- client/src/components/main-div/AddItems.js
+++ client/src/components/main-div/AddItems.js
@@ -1,19 +1,15 @@
-import React from "react";
+import { memo } from "react";
 import { BsPlusCircle } from "react-icons/bs";
 
 function AddItems({ handleAdd, dark }) {
 
-  function handleClick() {
-    return (e) => {
+  const handleClick = (e) => {
       e.preventDefault();
       e.stopPropagation();
       e.currentTarget.previousSibling.focus();
     };
-  }
   
-  function onEnter() {
-    return (e) => e.key === "Enter" && handleAdd(e);
-  }
+  const onEnter = (e) => e.key === "Enter" && handleAdd(e);
 
   return (
     <section className={`w-80 mx-auto mt-4 mb-5 pl-3 pt-3 pb-5 add-item border rounded-lg ${dark?'bg-gray-900 border-gray-700 text-white':'bg-gray-50 border-gray-300'} shadow-sm `}>
@@ -31,12 +27,12 @@ function AddItems({ handleAdd, dark }) {
           type="text"
           id="new-item"
           className={` ${dark?'placeholder-gray-400 text-white border-gray-600 bg-gray-700 bg-opacity-70 ':'text-gray-900 border-gray-300 bg-gray-50 '} inline-block p-4 border rounded-lg sm:text-md focus:ring-blue-500 focus:border-blue-500  `}
-          onKeyDown={onEnter()}
+          onKeyDown={onEnter}
         />
         <button
         title="Add Item"
           className={`${dark?'hover:text-white':'hover:text-gray-900'} inline-block p-3 text-center text-gray-500  `}
-          onClick={handleClick()}
+          onClick={handleClick}
         >
           {(
             <BsPlusCircle className={`${dark?' active:ring-gray-700 text-gray-200 border-gray-600 hover:text-gray-100 opacity-70 ':'text-gray-900 border-gray-200 active:ring-gray-200 hover:text-blue-700 '} w-8 h-8 mx-auto  active:outline-none rounded-full border active:z-10 active:ring-2  
@@ -48,4 +44,4 @@ function AddItems({ handleAdd, dark }) {
   );
 }
 
-export default React.memo(AddItems);
+export default memo(AddItems);
