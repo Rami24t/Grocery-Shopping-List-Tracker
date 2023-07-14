8c13d4a0ce590300c9d3f44c33cc18473bbc1a47
Rami <alsaadi.rami24@gmail.com>
2023-07-14 05:36:10 +0200

    wrap app in Context provider

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import ContextProvider from "./components/Context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ContextProvider>
+      <App />
+    </ContextProvider>  
   </React.StrictMode>
 );
