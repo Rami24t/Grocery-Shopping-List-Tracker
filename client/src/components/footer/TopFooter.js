428206d38a5ab828b265a9976964f1e60da63ef2
Rami <alsaadi.rami24@gmail.com>
2023-07-15 02:24:54 +0200

    update logo

--- client/src/components/footer/TopFooter.js
+++ client/src/components/footer/TopFooter.js
@@ -5,7 +5,7 @@ import FooterLists from "./FooterLists";
 function TopFooter( {dark} ) {
   return <div className="footer-top flex flex-col md:flex-row flex-wrap gap-3 justify-between">
     <Company
-      logo="/logo-no-background.svg"
+      logo="/logo2-no-background.svg"
       name="Rami Al-Saadi"
       link="https://www.linkedin.com/in/rami-al-saadi-16a14223a/" 
       dark={dark} 
