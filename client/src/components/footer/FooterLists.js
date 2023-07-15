d5e092519edac381ea961c1f59527aae4469fef3
Rami <alsaadi.rami24@gmail.com>
2023-07-15 18:12:04 +0200

    add React and Tailwing icons

--- client/src/components/footer/FooterLists.js
+++ client/src/components/footer/FooterLists.js
@@ -1,28 +1,38 @@
 import React from "react";
 import FooterList from "./FooterList";
+import { LiaReact } from "react-icons/lia";
+import { BiLogoTailwindCss } from "react-icons/bi";
 
-function FooterLists( {dark} ) {
-  return <div className="footer-lists flex gap-20 justify-around items-start">
-    <FooterList
-     dark={dark} 
-      title="Resources"
-      links={[
-        { text: "Rami", url: "https://www.github.com/rami24t" },
-        { text: "Tailwind CSS", url: "https://tailwindcss.com/" },
-        { text: "React", url: "https://react.dev/" },
-      ]} />
-    <FooterList
-     dark={dark} 
-      title="Follow me"
-      links={[
-        { text: "Github", url: "https://www.github.com/rami24t" },
-        {
-          text: "LinkedIn",
-          url: "https://www.linkedin.com/in/rami-al-saadi-16a14223a/",
-        },
-      ]} />
-    {/* <FooterList title="Legal" links={[{ text: "Privacy Policy" }, { text: "Terms & Conditions" }]} /> */}
-  </div>;
+function FooterLists({ dark }) {
+  return (
+    <div className="footer-lists flex gap-20 justify-around items-start">
+      <FooterList
+        dark={dark}
+        title="Resources"
+        links={[
+          { text: "Rami", url: "https://www.github.com/rami24t" },
+          {
+            text: "Tailwind CSS",
+            url: "https://tailwindcss.com/",
+            Logo: BiLogoTailwindCss,
+          },
+          { text: "React", url: "https://react.dev/", Logo: LiaReact },
+        ]}
+      />
+      <FooterList
+        dark={dark}
+        title="Follow me"
+        links={[
+          { text: "Github", url: "https://www.github.com/rami24t" },
+          {
+            text: "LinkedIn",
+            url: "https://www.linkedin.com/in/rami-al-saadi-16a14223a/",
+          },
+        ]}
+      />
+      {/* <FooterList title="Legal" links={[{ text: "Privacy Policy" }, { text: "Terms & Conditions" }]} /> */}
+    </div>
+  );
 }
 
 export default React.memo(FooterLists);
