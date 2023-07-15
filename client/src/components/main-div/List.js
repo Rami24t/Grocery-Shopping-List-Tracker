252d8f9c4a1e6516e3466f47b522a2470d3da8de
Rami <alsaadi.rami24@gmail.com>
2023-07-15 05:59:51 +0200

    UI - improve styles - add feedback (in)visible eye

--- client/src/components/main-div/List.js
+++ client/src/components/main-div/List.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Item from "./item/Item";
+import { BsFillEyeSlashFill, BsFillEyeFill } from "react-icons/bs";
 
 function List({
   title,
@@ -17,15 +18,16 @@ function List({
   return (
     <article>
       <h3
-        className={`max-w-max mx-auto cursor-pointer font-bold ${
+        className={`flex justify-between min-w-[154px] items-center gap-2 max-w-max mx-auto cursor-pointer font-bold ${
           items[0].need ? "text-orange-200" : "text-teal-300 "
         }`}
         onClick={() => setShowItems(!showItems)}
       >
         {title}: {items.length}
+        {showItems ? <BsFillEyeFill className="inline-block"/>: <BsFillEyeSlashFill className="inline" />}
         <div
-          className={`ms-1 p-2 cursor-pointer transition-transform inline-flex text-center items-center justify-center w-4 h-4 ${
-            showItems ? "rotate-90" : "-rotate-0 transform -translate-y-0.5"
+          className={`p-2 cursor-pointer text-lg transition-transform inline-flex text-center items-center justify-center w-4 h-4 ${
+            showItems ? "rotate-90 translate-x-0.5" : "-rotate-0 transform -translate-y-0.5"
           }`}
         >
           &gt;
--- client/src/components/main-div/item/Item.js
+++ client/src/components/main-div/item/Item.js
@@ -54,7 +54,7 @@ const Item = ({ item, handleDelete, handleToggle, updateItem, dark }) => {
         onChange={() => handleCheck(item)}
         checked={!needed}
         id="needed"
-        className={`opacity-95 cursor-pointer inline-block leading-none w-6 h-6 m-2 border rounded focus:ring-3 ${!dark?'border-gray-300 bg-gray-50 focus:ring-blue-300':' bg-gray-700 border-gray-600 focus:ring-blue-600 ring-offset-gray-800 focus:ring-offset-gray-800'}`}
+        className={`opacity-75 filter  ${needed?'invert hue-rotate-180 brightness-75 ':''} cursor-pointer inline-block leading-none w-5 h-5 m-2 border rounded focus:ring-3 ${!dark?'border-gray-300 bg-gray-50 focus:ring-blue-300':' bg-gray-700 border-gray-600 focus:ring-blue-600 ring-offset-gray-800 focus:ring-offset-gray-800'}`}
         title={`Click to ${needed ? "check" : "uncheck"}`}
       />
     );
@@ -64,10 +64,10 @@ const Item = ({ item, handleDelete, handleToggle, updateItem, dark }) => {
       <button
         type="button"
         onClick={handleEdit}
-        className={`leading-none w-6 h-6 rounded-lg m-2 text-center border hover:font-bold text-xl focus:ring-4 focus:outline-none  ${dark?'border-slate-500 text-slate-400 focus:ring-slate-800 hover:text-slate-300 hover:border-slate-400': ' text-slate-800 hover:text-slate-600   border-slate-600 focus:ring-slate-400 '}`}
+        className={`leading-none w-6 h-6 rounded-lg m-2 text-center border hover:font-bold text-xl focus:ring-4 focus:outline-none  ${dark?'border-indigo-400 bg-indigo-950 text-indigo-300 focus:ring-indigo-700 hover:text-indigo-200 hover:border-indigo-300': ' text-slate-800 hover:text-slate-600   border-slate-600 focus:ring-slate-400 '}`}
         title="Click to edit this item"
       >
-        <BsPencil className="pointer-events-none " />
+        <BsPencil className="pointer-events-none" />
       </button>
     );
   }
@@ -76,10 +76,10 @@ const Item = ({ item, handleDelete, handleToggle, updateItem, dark }) => {
       <button
         onClick={() => handleDelete(item)}
         type="button"
-        className={`inline-block w-6 h-6 pb-2 leading-none rounded-lg ml-3 m-2 text-center hover:border-red-500 hover:font-bold text-xl border focus:ring-4 focus:outline-none ${dark?'border-red-600 text-red-500 focus:ring-red-900 hover:text-red-400':' text-red-700 hover:text-red-500 border-red-700 focus:ring-red-300'} `}
+        className={`inline-flex items-center justify-center w-6 h-6 leading-none rounded-lg ml-3 m-2 text-center hover:border-red-500 hover:font-bold text-xl border focus:ring-4 focus:outline-none ${dark?'border-red-600 hover:border-red-400 text-red-500 bg-red-950  focus:ring-red-900 hover:text-red-400':' text-red-700 hover:text-red-500 border-red-700 focus:ring-red-300'} `}
         title="Click to permanently delete this item"
       >
-        x
+        <span className="mb-1">x</span>
       </button>
     );
   }
