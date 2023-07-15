c76d28a9b23d2215604b0929f769a8cdc24ce98b
Rami <alsaadi.rami24@gmail.com>
2023-07-15 18:11:23 +0200

    add icons to FooterList component

--- client/src/components/footer/FooterListItem.js
+++ client/src/components/footer/FooterListItem.js
@@ -1,16 +1,21 @@
 import React from "react";
 
 function FooterListItem({ link }) {
-  return <li className="mb-4">
-    <a
-      href={link.url}
-      className="hover:underline"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      {link.text}
-    </a>
-  </li>;
+  return (
+    <li className="mb-4">
+      <a
+        href={link.url}
+        className="hover:underline"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <span className="flex items-center gap-2">
+          {link.text}
+          {link.Logo ? <link.Logo className="inline" /> : null}
+        </span>
+      </a>
+    </li>
+  );
 }
 
 export default React.memo(FooterListItem);
