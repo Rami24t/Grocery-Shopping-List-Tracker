2cb157e237c97abf04647ec6b29f9e81b3f2f231
Rami <alsaadi.rami24@gmail.com>
2023-07-16 23:15:06 +0200

    edit badges colors/hover

--- client/src/components/aside-left/LIBadges.js
+++ client/src/components/aside-left/LIBadges.js
@@ -5,7 +5,7 @@ const badgesCommonStyle = 'px-2 inline-flex items-center justify-center text-sm 
 export const ProBadge = React.memo(({ dark }) => (
     <span
       className={` ${dark
-          ? "bg-gray-700 hover:bg-gray-800 text-gray-300"
+          ? "bg-gray-800 hover:bg-gray-600 text-gray-300"
           : "text-gray-800 bg-gray-200 "} sm:ml-3 pb-[1px] ${badgesCommonStyle}}`}
     >
       Pro
@@ -15,7 +15,7 @@ export const ProBadge = React.memo(({ dark }) => (
 export const ItemsBadge = React.memo(({ items, dark }) => (
     <span
       className={` ${
-        dark ? "bg-blue-900 text-blue-300" : "text-blue-800 bg-blue-100 "
+        dark ? "bg-blue-950 hover:bg-blue-800 text-blue-300" : "text-blue-800 bg-blue-100 "
       } sm:ml-3 mr-1 w-3 h-3 py-2 ${badgesCommonStyle} `}
     >
       {items}
