1043329e7573fbcb68ad31e114c59c244ba40325
Rami <alsaadi.rami24@gmail.com>
2023-07-18 00:27:11 +0200

    add html bg / format code

--- client/src/components/aside-left/LIBadges.js
+++ client/src/components/aside-left/LIBadges.js
@@ -1,23 +1,28 @@
 import React from "react";
 
-const badgesCommonStyle = 'px-2 inline-flex items-center justify-center text-sm font-medium rounded-full px-2 inline-flex items-center justify-center text-sm font-medium rounded-full';
+const badgesCommonStyle =
+  "px-2 inline-flex items-center justify-center text-sm font-medium rounded-full px-2 inline-flex items-center justify-center text-sm font-medium rounded-full";
 
 export const ProBadge = React.memo(({ dark }) => (
-    <span
-      className={` ${dark
-          ? "bg-gray-800 hover:bg-gray-600 text-gray-300"
-          : "text-gray-800 bg-gray-200 "} sm:ml-3 pb-[1px] ${badgesCommonStyle}}`}
-    >
-      Pro
-    </span>
-  ));
+  <span
+    className={` ${
+      dark
+        ? "bg-gray-800 hover:bg-gray-600 text-gray-300"
+        : "text-gray-800 bg-gray-200 "
+    } sm:ml-3 pb-[1px] ${badgesCommonStyle}}`}
+  >
+    Pro
+  </span>
+));
 
 export const ItemsBadge = React.memo(({ items, dark }) => (
-    <span
-      className={` ${
-        dark ? "bg-blue-950 hover:bg-blue-800 text-blue-300" : "text-blue-800 bg-blue-100 "
-      } sm:ml-3 mr-1 w-3 h-3 py-2 ${badgesCommonStyle} `}
-    >
-      {items}
-    </span>
-  ));
+  <span
+    className={` ${
+      dark
+        ? "bg-blue-950 hover:bg-blue-800 text-blue-300"
+        : "text-blue-800 bg-blue-100 "
+    } sm:ml-3 mr-1 w-3 h-3 py-2 ${badgesCommonStyle} `}
+  >
+    {items}
+  </span>
+));
--- client/src/components/aside-left/MenuButton.js
+++ client/src/components/aside-left/MenuButton.js
@@ -1,15 +1,18 @@
 import React from "react";
 import "./MenuButton.scss";
 
-const MenuButton = React.memo( ({ showSideNav, setShowSideNav,dark }) => {
+const MenuButton = React.memo(({ showSideNav, setShowSideNav, dark }) => {
   return (
     <div
       onClick={() => setShowSideNav((prev) => !prev)}
-     className={`rounded-bl-2xl bg-gray-950 bg-opacity-80 max-h-[60px] overflow-hidden cursor-pointer menu-btn z-50 ${showSideNav ? "open " : ""}`}>
+      className={`rounded-bl-2xl bg-gray-950 bg-opacity-80 max-h-[60px] overflow-hidden cursor-pointer menu-btn z-50 ${
+        showSideNav ? "open " : ""
+      }`}
+    >
       <div className={`ml-3 icon-left`}></div>
       <div className={`ml-3 icon-right`}></div>
     </div>
   );
-})
+});
 
 export default MenuButton;
