81716aaf306776189cb45f1d4377c2951b5a9c57
Rami <alsaadi.rami24@gmail.com>
2023-07-18 22:20:00 +0200

    refractor code debug

--- client/src/components/header/ShoppingBagSvgs.js
+++ client/src/components/header/ShoppingBagSvgs.js
@@ -13,7 +13,7 @@ const ShoppingBagFillSvgPath = (
   <path d="M17 5.923A1 1 0 0 0 16 5h-3V4a4 4 0 1 0-8 0v1H2a1 1 0 0 0-1 .923L.086 17.846A2 2 0 0 0 2.08 20h13.84a2 2 0 0 0 1.994-2.153L17 5.923ZM7 9a1 1 0 0 1-2 0V7h2v2Zm0-5a2 2 0 1 1 4 0v1H7V4Zm6 5a1 1 0 1 1-2 0V7h2v2Z" />
 );
 
-const Svg = React.memo(({ className, fill, path }) => (
+const Svg = ({ className, fill, path }) => (
   <svg
     className={className}
     aria-hidden="true"
@@ -23,7 +23,7 @@ const Svg = React.memo(({ className, fill, path }) => (
   >
     {path}
   </svg>
-));
+);
 
 const ShoppingBagSvg = React.memo(({ className }) =>
   Svg({ className, path: ShoppingBagSvgPath, fill: "none" })
