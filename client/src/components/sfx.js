5318d9044aa5c14a62d90dfa5ad681f9557d2d2e
Rami <alsaadi.rami24@gmail.com>
2023-07-20 22:08:59 +0200

    create to import files and export audio objects

---
+++ client/src/components/sfx.js
@@ -0,0 +1,103 @@
+import addSFX1 from "../assets/achive-sound-132273.mp3";
+import haveSFX from "../assets/beep-6-96243.mp3";
+import buttonSFX from "../assets/button-8-88355.mp3";
+import addSFX2 from "../assets/button-pressed-38129.mp3";
+import buttonClickSFX from "../assets/click-3-44710.mp3";
+import buttonClickSFX2 from "../assets/click-button-140881.mp3";
+import CompletionSFX from "../assets/click-game-menu-147356.mp3";
+import menuOpenSFX from "../assets/clickselect-92098.mp3";
+import finishSFX from "../assets/divinity-138419.mp3";
+import slideInSFX from "../assets/door-open-107728.mp3";
+import linkClickSFX from "../assets/light-switch-156813.mp3";
+import listCloseOpenSFX from "../assets/killswitch-being-turned-on-100591.mp3";
+import toBottomTopSFX from "../assets/uisound1-79819.mp3";
+import editClickSFX from "../assets/uiclick4-79818.mp3";
+import menuButtonClick from "../assets/ui-click-43196.mp3";
+import deleteSFX from "../assets/toy-button-105724.mp3";
+import wrongFilterSFX from "../assets/wrong-answer-129254.mp3";
+import correctOrAddSFX from "../assets/ui_correct_button2-103167.mp3";
+import openCloseAddFormSFX from "../assets/spring-click-sound-95507.mp3";
+import navLinkClickSFX from "../assets/switch-light-04-82204.mp3";
+import slideOutInSFX from "../assets/sliding_wooden_cabinet_door_1-of-2-81503.mp3";
+import clearSFX from "../assets/error-2-126514.mp3";
+import addDeniedSFX from "../assets/select_denied_03-45945.mp3";
+import writingSFX from "../assets/scribbling-68130.mp3";
+import openListorClearSFX from "../assets/projector-button-push-6258.mp3";
+import typeSFX from "../assets/mech-keyboard-02-102918.mp3";
+import resetSFX from "../assets/ping-82822.mp3";
+import pencilCheckSFX from "../assets/pencil_check_mark_1-88805.mp3";
+import mouseClickSFX from "../assets/mouse-click-153941.mp3";
+import resetOrClearFilterSFX from "../assets/fantasy_ui_button_6-102219.mp3";
+import wrongFilterSFX2 from "../assets/error-when-entering-the-game-menu-132111.mp3";
+import inputErrorSFX from "../assets/error-call-to-attention-129258.mp3";
+import errorSFX from "../assets/error-call-to-attention-129258.mp3";
+
+const addSFXAudio1 = new Audio(addSFX1);
+const haveSFXAudio = new Audio(haveSFX);
+const buttonSFXAudio = new Audio(buttonSFX);
+const addSFXAudio2 = new Audio(addSFX2);
+const buttonClickSFXAudio = new Audio(buttonClickSFX);
+const buttonClickSFXAudio2 = new Audio(buttonClickSFX2);
+const completionSFXAudio = new Audio(CompletionSFX);
+const menuOpenSFXAudio = new Audio(menuOpenSFX);
+const finishSFXAudio = new Audio(finishSFX);
+const slideInSFXAudio = new Audio(slideInSFX);
+const linkClickSFXAudio = new Audio(linkClickSFX);
+const listCloseOpenSFXAudio = new Audio(listCloseOpenSFX);
+const toBottomTopSFXAudio = new Audio(toBottomTopSFX);
+const editClickSFXAudio = new Audio(editClickSFX);
+const menuButtonClickAudio = new Audio(menuButtonClick);
+const deleteSFXAudio = new Audio(deleteSFX);
+const wrongFilterSFXAudio = new Audio(wrongFilterSFX);
+const wrongFilterSFXAudio2 = new Audio(wrongFilterSFX2);
+const correctOrAddSFXAudio = new Audio(correctOrAddSFX);
+const openCloseAddFormSFXAudio = new Audio(openCloseAddFormSFX);
+const navLinkClickSFXAudio = new Audio(navLinkClickSFX);
+const slideOutInSFXAudio = new Audio(slideOutInSFX);
+const clearSFXAudio = new Audio(clearSFX);
+const addDeniedSFXAudio = new Audio(addDeniedSFX);
+const writingSFXAudio = new Audio(writingSFX);
+const openListorClearSFXAudio = new Audio(openListorClearSFX);
+const typeSFXAudio = new Audio(typeSFX);
+const resetSFXAudio = new Audio(resetSFX);
+const pencilCheckSFXAudio = new Audio(pencilCheckSFX);
+const mouseClickSFXAudio = new Audio(mouseClickSFX);
+const resetOrClearFilterSFXAudio = new Audio(resetOrClearFilterSFX);
+const inputErrorSFXAudio = new Audio(inputErrorSFX);
+const errorSFXAudio = new Audio(errorSFX);
+
+export {
+  addSFXAudio1,
+  haveSFXAudio,
+  buttonSFXAudio,
+  addSFXAudio2,
+  buttonClickSFXAudio,
+  buttonClickSFXAudio2,
+  completionSFXAudio,
+  menuOpenSFXAudio,
+  finishSFXAudio,
+  slideInSFXAudio,
+  linkClickSFXAudio,
+  listCloseOpenSFXAudio,
+  toBottomTopSFXAudio,
+  editClickSFXAudio,
+  menuButtonClickAudio,
+  deleteSFXAudio,
+  wrongFilterSFXAudio,
+  wrongFilterSFXAudio2,
+  correctOrAddSFXAudio,
+  openCloseAddFormSFXAudio,
+  navLinkClickSFXAudio,
+  slideOutInSFXAudio,
+  clearSFXAudio,
+  addDeniedSFXAudio,
+  writingSFXAudio,
+  openListorClearSFXAudio,
+  typeSFXAudio,
+  resetSFXAudio,
+  pencilCheckSFXAudio,
+  mouseClickSFXAudio,
+  resetOrClearFilterSFXAudio,
+  inputErrorSFXAudio,
+  errorSFXAudio,
+};
