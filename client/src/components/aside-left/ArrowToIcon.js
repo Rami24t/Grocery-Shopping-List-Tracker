dbb1c7ec538a358e2427a3036dea044a0eefb2b7
Rami <alsaadi.rami24@gmail.com>
2023-07-21 02:08:27 +0200

    add sound effects

--- client/src/components/aside-left/ArrowToIcon.js
+++ client/src/components/aside-left/ArrowToIcon.js
@@ -1,9 +1,31 @@
 import React from "react";
 import { BiSolidArrowToTop } from "react-icons/bi";
+import {
+  linkClickSFXAudio,
+  navLinkClickSFXAudio,
+  buttonClickSFXAudio,
+  buttonClickSFXAudio2,
+  slideOutInSFXAudio,
+} from "../../utils/sfx";
 
 const ArrowToIcon = React.memo(({ toBottom, dark, shown }) => (
   <a
     href={`${toBottom ? "#footer" : "#app"}`}
+    onClick={(e) => {
+      e.stopPropagation();
+      e.target.style.pointerEvents = "none";
+      setTimeout(() => {
+        e.target.style.pointerEvents = "";
+      }, 800);
+      buttonClickSFXAudio.currentTime = 0;
+      buttonClickSFXAudio.play();
+      slideOutInSFXAudio.currentTime = 0;
+      slideOutInSFXAudio.play();
+      setTimeout(() => {
+        slideOutInSFXAudio.pause();
+        slideOutInSFXAudio.currentTime = 0;
+      }, 550);
+    }}
     className={`text-xl rounded-e-xl bg-gray-900 bg-opacity-80 flex items-center justify-center fixed z-40  ${
       shown ? "-left-0.5" : "-left-8"
     } ${
