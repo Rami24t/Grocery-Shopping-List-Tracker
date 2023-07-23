52466077bf0b7eb575825f120a9db192d060f91c
Rami <alsaadi.rami24@gmail.com>
2023-07-23 02:55:47 +0200

    refractor & add scroll mt-3

--- client/src/components/main-div/filter-section/FilterInput.js
+++ client/src/components/main-div/filter-section/FilterInput.js
@@ -1,19 +1,12 @@
 import React from "react";
-import { writingSFXAudio, editClickSFXAudio } from "../../../utils/sfx";
+import { writingSFXAudio, editClickSFXAudio, playSFXAudio } from "../../../utils/sfx";
 
 function FilterInput({ filter, handleChangeFilter, items, dark }) {
   function onEnter(e) {
     if (e.key === "Enter") e.target.blur();
   }
   function handleFocus() {
-    try {
-      editClickSFXAudio.currentTime = 0;
-      editClickSFXAudio.play();
-      writingSFXAudio.currentTime = 0;
-      writingSFXAudio.play();
-    } catch (error) {
-      console.log(error);
-    }
+    playSFXAudio(editClickSFXAudio, writingSFXAudio);
   }
   function handleBlur() {
     writingSFXAudio.pause();
@@ -33,7 +26,7 @@ function FilterInput({ filter, handleChangeFilter, items, dark }) {
       onFocus={handleFocus}
       onBlur={handleBlur}
       onKeyDown={onEnter}
-      className={`ms-0.5 w-56 md:w-64 lg:w-80 border text-sm rounded-lg focus:ring-blue-500  focus:border-blue-500 block pl-7 p-2.5 ${
+      className={`scroll-mt-3 ms-0.5 w-56 md:w-64 lg:w-80 border text-sm rounded-lg focus:ring-blue-500  focus:border-blue-500 block pl-7 p-2.5 ${
         !dark
           ? "bg-gray-50 border-gray-300 text-gray-900"
           : "placeholder-gray-400 bg-gray-700 bg-opacity-70 border-gray-600 text-white"
