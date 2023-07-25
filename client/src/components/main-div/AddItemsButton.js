66e84c562f8b7d6a17b38ec332d40007a051b95e
Rami <alsaadi.rami24@gmail.com>
2023-07-25 22:48:21 +0200

    Edit styles - Edit light mode colors.

--- client/src/components/main-div/AddItemsButton.js
+++ client/src/components/main-div/AddItemsButton.js
@@ -1,17 +1,27 @@
 import React, { useEffect, useState } from "react";
-import { openCloseAddFormSFX, openCloseAddFormSFXAudio, playSFXAudio } from "../../utils/sfx";
+import {
+  openCloseAddFormSFX,
+  openCloseAddFormSFXAudio,
+  playSFXAudio,
+} from "../../utils/sfx";
 import useAudio from "../../hooks/useAudio";
 import { playAudio } from "../../utils/playAudio";
 
 const BUTTON_COOLDOWN_MS = 900;
 let INITIAL_AUDIO_CONTEXT;
 try {
-  INITIAL_AUDIO_CONTEXT = new (window.AudioContext || window.webkitAudioContext)();
+  INITIAL_AUDIO_CONTEXT = new (window.AudioContext ||
+    window.webkitAudioContext)();
 } catch (error) {
   console.log(error);
 }
 
-function AddItemsButton({ showAddItem, setShowAddItem, listIsEmpty, darkMode }) {
+function AddItemsButton({
+  showAddItem,
+  setShowAddItem,
+  listIsEmpty,
+  darkMode,
+}) {
   const { reversedBuffer } = useAudio(openCloseAddFormSFX);
   const [buttonDisabled, setButtonDisabled] = useState(false);
 
@@ -40,26 +50,36 @@ function AddItemsButton({ showAddItem, setShowAddItem, listIsEmpty, darkMode }) 
     setShowAddItem((prev) => !prev);
   };
 
-  
   return (
     <button
       disabled={listIsEmpty || buttonDisabled}
       onClick={onClickHandler}
       id="add-item-button"
-      className={`font-semibold transition-all inline-flex items-center justify-between gap-1.5 w-10 relative rounded-lg ${
+      className={`${
+        darkMode
+          ? "text-slate-200 hover:text-slate-100"
+          : "text-slate-100 hover:text-slate-50"
+      } font-semibold transition-all inline-flex items-center justify-between gap-1.5 w-10 relative rounded-lg ${
         !showAddItem
-          ? "bg-indigo-700  hover:bg-indigo-600"
+          ? darkMode
+            ? "bg-indigo-700 hover:bg-indigo-600"
+            : "bg-blue-700 hover:bg-blue-800 "
           : listIsEmpty
-          ? "bg-red-950 "
-          : "bg-red-800  hover:bg-red-700"
-      } px-3 py-2 text-xs font-medium text-slate-200 transition focus:outline-none focus:ring hover:text-indigo-100 `}
+          ? darkMode
+            ? "bg-red-950 hidden"
+            : "hidden"
+          : darkMode
+          ? "bg-red-800  hover:bg-red-700"
+          : "bg-red-600  hover:bg-red-700"
+      }
+          px-3 py-2 text-xs font-medium  transition focus:outline-none focus:ring  `}
       type="button"
     >
       {"Add "}
       <div
-        className={`font-semibold transition-transform flex text-center items-center justify-center w-3 h-3 ${
-          !darkMode ? "text-gray-800" : "text-white -translate-y-0.5"
-        } ${showAddItem ? "duration-500 rotate-90" : "rotate-0"}`}
+        className={`font-semibold transition-transform flex text-center items-center justify-center w-3 h-3 -translate-y-0.5 ${
+          showAddItem ? "duration-500 rotate-90" : "rotate-0"
+        }`}
       >
         &gt;
       </div>
