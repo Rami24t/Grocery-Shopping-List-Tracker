afa72ed89f8fede8ec491dc4aa5bbac7d002eed5
Rami <alsaadi.rami24@gmail.com>
2023-07-25 22:48:38 +0200

    Edit styles - Edit light mode colors.

--- client/src/components/main-div/ResetButton.js
+++ client/src/components/main-div/ResetButton.js
@@ -3,30 +3,38 @@ import React from "react";
 function ResetButton({ items, handleClear, handleReset, darkMode }) {
   return (
     <button
-      onClick={(e)=>{
+      onClick={(e) => {
         items.length ? handleClear(e) : handleReset(e);
         e.target.disabled = true;
         setTimeout(() => {
           e.target.disabled = false;
         }, 900);
       }}
-      className={`font-semibold transition-all w-10 relative rounded-lg
+      className={`${
+        darkMode
+          ? "text-slate-200 hover:text-slate-100"
+          : "text-slate-100 hover:text-slate-50"
+      } font-semibold transition-all w-10 relative rounded-lg
       ${
         !items.length
-          ? "bg-indigo-700  hover:bg-indigo-600"
-          : "bg-red-800  hover:bg-red-700"
-      } px-3 py-2 text-xs font-medium text-slate-200 transition focus:outline-none focus:ring
-       hover:text-indigo-100
-      `}
+          ? darkMode
+            ? "bg-indigo-700  hover:bg-indigo-600"
+            : "bg-indigo-600  hover:bg-indigo-700"
+          : darkMode
+          ? "bg-red-800  hover:bg-red-700"
+          : "bg-red-600  hover:bg-red-700"
+      } px-3 py-2 text-xs font-medium transition focus:outline-none focus:ring`}
       type="button"
     >
       {items.length ? "Clear" : "Reset"}
       <div
-        className={`aspect-square rounded-full px-[3px] py-[2px] pt-[5px] bg-indigo-500 transition-transform flex text-center items-center justify-center min-w-[12px] min-h-[12px] font-bold ${
-          darkMode ? "border-gray-800 text-gray-50" : "border-white text-gray-800"
+        className={`aspect-square rounded-full px-[3px] py-[2px] pt-[5px] transition-transform flex text-center items-center justify-center min-w-[12px] min-h-[12px] font-bold ${
+          darkMode
+            ? "border-gray-800 text-gray-50 bg-indigo-500 "
+            : "bg-indigo-200 border-indigo-100 text-gray-700"
         } ${
           !items.length ? "rotate-90" : "rotate-0"
-        } absolute flex items-center justify-center text-xs font-bold text-white  border-2 rounded-full -top-2 -right-2`}
+        } absolute flex items-center justify-center text-xs font-bold border-2 rounded-full -top-2 -right-2`}
       >
         {items.length || "O"}
       </div>
