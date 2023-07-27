5980383777839ce616c5d92c047e3d56326c322c
Rami <alsaadi.rami24@gmail.com>
2023-07-27 16:28:27 +0200

    add readonly and proper jsx syntax

--- client/src/components/header/ToggleDarkModeButton.js
+++ client/src/components/header/ToggleDarkModeButton.js
@@ -3,24 +3,25 @@ import React from "react";
 function ToggleDarkModeButton({ darkMode, handleClick }) {
   
   return  <>
-  <button onClick={handleClick} type="button" class={`-mt-11 float-right ml-3 px-1 cursor-pointer flex items-center w-9 h-9 justify-center text-xs font-medium border rounded-lg toggle-dark-state-example focus:z-10 focus:ring-2 focus:outline-none 
+  <button onClick={handleClick} type="button" className={`-mt-11 float-right ml-3 px-1 cursor-pointer flex items-center w-9 h-9 justify-center text-xs font-medium border rounded-lg toggle-dark-state-example focus:z-10 focus:ring-2 focus:outline-none 
   ${!darkMode?"hover:bg-gray-100 hover:text-blue-800 text-blue-600 bg-gray-100 border-gray-200 focus:ring-gray-300": 
   "focus:ring-gray-500 bg-gray-800 text-gray-400 border-gray-700 hover:text-gray-100 hover:bg-gray-700"}`}>
     {
       !darkMode?
-    <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
+    <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
       <path d="M17.8 13.75a1 1 0 0 0-.859-.5A7.488 7.488 0 0 1 10.52 2a1 1 0 0 0 0-.969A1.035 1.035 0 0 0 9.687.5h-.113a9.5 9.5 0 1 0 8.222 14.247 1 1 0 0 0 .004-.997Z"></path>
     </svg>
     :
-    <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+    <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
       <path d="M10 15a5 5 0 1 0 0-10 5 5 0 0 0 0 10Zm0-11a1 1 0 0 0 1-1V1a1 1 0 0 0-2 0v2a1 1 0 0 0 1 1Zm0 12a1 1 0 0 0-1 1v2a1 1 0 1 0 2 0v-2a1 1 0 0 0-1-1ZM4.343 5.757a1 1 0 0 0 1.414-1.414L4.343 2.929a1 1 0 0 0-1.414 1.414l1.414 1.414Zm11.314 8.486a1 1 0 0 0-1.414 1.414l1.414 1.414a1 1 0 0 0 1.414-1.414l-1.414-1.414ZM4 10a1 1 0 0 0-1-1H1a1 1 0 0 0 0 2h2a1 1 0 0 0 1-1Zm15-1h-2a1 1 0 1 0 0 2h2a1 1 0 0 0 0-2ZM4.343 14.243l-1.414 1.414a1 1 0 1 0 1.414 1.414l1.414-1.414a1 1 0 0 0-1.414-1.414ZM14.95 6.05a1 1 0 0 0 .707-.293l1.414-1.414a1 1 0 1 0-1.414-1.414l-1.414 1.414a1 1 0 0 0 .707 1.707Z"></path>
     </svg>
     }
-    <span class="sr-only">Toggle dark/light mode</span>
+    <span className="sr-only">Toggle dark/light mode</span>
     <label className="relative ml-1 inline-flex items-center cursor-pointer">
             <input
               type="checkbox"
               checked={!darkMode}
+              readOnly={true}
               className="m-0 p-0 sr-only peer"
             />
             <div
