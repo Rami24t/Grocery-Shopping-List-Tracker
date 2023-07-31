7878a06cd3686beab3df93e2884cd6fbabe6385e
Rami <alsaadi.rami24@gmail.com>
2023-07-31 02:17:44 +0200

    edit text colors in light-mode

--- client/src/components/footer/CopyrightDisclaimer.js
+++ client/src/components/footer/CopyrightDisclaimer.js
@@ -12,14 +12,14 @@ function CopyrightDisclaimer({
     playSFXAudio(sfx);
   };
   return (
-    <span className={`copyright-disclaimer mt-1 text-sm  sm:text-center ${darkMode?"text-gray-500":"text-gray-500"}`}>
+    <span className={`copyright-disclaimer mt-1 text-sm  sm:text-center ${darkMode?"text-gray-500":"text-gray-600"}`}>
       {`© ${year} `}
       <a
         href={link}
         onClick={handleClick}
         target="_blank"
         rel="noopener noreferrer"
-        className={` ${darkMode?"text-gray-500  hover:text-gray-400":"text-gray-500"} hover:underline`}  
+        className={`${darkMode?"hover:text-gray-400":"text-gray-700 hover:text-gray-800"} hover:underline`}  
       >
         {author}
       </a>
