4f28d0355c8880281c552aad6cf4783228ae5f10
Rami <alsaadi.rami24@gmail.com>
2023-07-31 22:26:52 +0200

    refractor code - mobe imgs/add imgs index

---
+++ client/src/assets/deco-imgs/decoImgs.js
@@ -0,0 +1,150 @@
+import img1 from "./deco (1).jpg";
+import img2 from "./deco (2).jpg";
+import img3 from "./deco (3).jpg";
+import img4 from "./deco (4).jpg";
+import img5 from "./deco (5).jpg";
+import img6 from "./deco (6).jpg";
+import img7 from "./deco (7).jpg";
+import img8 from "./deco (8).jpg";
+import img9 from "./deco (9).jpg";
+import img10 from "./deco (10).jpg";
+import img11 from "./deco (11).jpg";
+import img12 from "./deco (12).jpg";
+import img13 from "./deco (13).jpg";
+import img14 from "./deco (14).jpg";
+import img15 from "./deco (15).jpg";
+import img16 from "./deco (16).jpg";
+import img17 from "./deco (17).jpg";
+import img18 from "./deco (18).jpg";
+import img19 from "./deco (19).jpg";
+import img20 from "./deco (20).jpg";
+import img21 from "./deco (21).jpg";
+import img22 from "./deco (22).jpg";
+import img23 from "./deco (23).jpg";
+import img24 from "./deco (24).jpg";
+import img25 from "./deco (25).jpg";
+import img26 from "./deco (26).jpg";
+import img27 from "./deco (27).jpg";
+import img28 from "./deco (28).jpg";
+import img29 from "./deco (29).jpg";
+import img30 from "./deco (30).jpg";
+import img31 from "./deco (31).jpg";
+import img32 from "./deco (32).jpg";
+import img33 from "./deco (33).jpg";
+import img34 from "./deco (34).jpg";
+import img35 from "./deco (35).jpg";
+import img36 from "./deco (36).jpg";
+import img37 from "./deco (37).jpg";
+import img38 from "./deco (38).jpg";
+import img39 from "./deco (39).jpg";
+import img40 from "./deco (40).jpg";
+import img41 from "./deco (41).jpg";
+import img42 from "./deco (42).jpg";
+import img43 from "./deco (43).jpg";
+import img44 from "./deco (44).jpg";
+import img45 from "./deco (45).jpg";
+import img46 from "./deco (46).jpg";
+import img47 from "./deco (47).jpg";
+import img48 from "./deco (48).jpg";
+import img49 from "./deco (49).jpg";
+import img50 from "./deco (50).jpg";
+import img51 from "./deco (51).jpg";
+import img52 from "./deco (52).jpg";
+import img53 from "./deco (53).jpg";
+import img54 from "./deco (54).jpg";
+import img55 from "./deco (55).jpg";
+import img56 from "./deco (56).jpg";
+import img57 from "./deco (57).jpg";
+import img58 from "./deco (58).jpg";
+import img59 from "./deco (59).jpg";
+import img60 from "./deco (60).jpg";
+import img61 from "./deco (61).jpg";
+import img62 from "./deco (62).jpg";
+import img63 from "./deco (63).jpg";
+import img64 from "./deco (64).jpg";
+import img65 from "./deco (65).jpg";
+import img66 from "./deco (66).jpg";
+import img67 from "./deco (67).jpg";
+import img68 from "./deco (68).jpg";
+import img69 from "./deco (69).jpg";
+import img70 from "./deco (70).jpg";
+import img71 from "./deco (71).jpg";
+import img72 from "./deco (72).jpg";
+
+const decoImgs = [
+  "https://source.unsplash.com/random/380x380/?vegetables?",
+  img1,
+  img2,
+  img3,
+  img4,
+  img5,
+  img6,
+  img7,
+  img8,
+  img9,
+  img10,
+  img11,
+  img12,
+  img13,
+  img14,
+  img15,
+  img16,
+  img17,
+  img18,
+  img19,
+  img20,
+  img21,
+  img22,
+  img23,
+  img24,
+  img25,
+  img26,
+  img27,
+  img28,
+  img29,
+  img30,
+  img31,
+  img32,
+  img33,
+  img34,
+  img35,
+  img36,
+  img37,
+  img38,
+  img39,
+  img40,
+  img41,
+  img42,
+  img43,
+  img44,
+  img45,
+  img46,
+  img47,
+  img48,
+  img49,
+  img50,
+  img51,
+  img52,
+  img53,
+  img54,
+  img55,
+  img56,
+  img57,
+  img58,
+  img59,
+  img60,
+  img61,
+  img62,
+  img63,
+  img64,
+  img65,
+  img66,
+  img67,
+  img68,
+  img69,
+  img70,
+  img71,
+  img72,
+];
+
+export default decoImgs;
