2982ecf23d3b2f16568f798794ec20ac0581cc1d
Rami <alsaadi.rami24@gmail.com>
2023-08-11 20:16:33 +0200

    add logo-svg in assets and import to TopFooter

--- client/src/components/footer/TopFooter.js
+++ client/src/components/footer/TopFooter.js
@@ -2,11 +2,12 @@ import React from "react";
 import Company from "./Company";
 import FooterLists from "./FooterLists";
 import { navLinkClickSFXAudio } from "../../assets/sfx";
+import logoSvg from "../../assets/logo/logo.svg";
 
 function TopFooter( {darkMode} ) {
   return <div className="items-center footer-top flex flex-col md:flex-row flex-wrap gap-3 justify-between">
     <Company
-      logo="/logo2-no-background.svg"
+      logo={logoSvg}
       name="Rami Al-Saadi"
       link="https://www.linkedin.com/in/rami-al-saadi-16a14223a/" 
       sfx={navLinkClickSFXAudio}
