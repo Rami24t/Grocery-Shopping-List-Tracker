fd8e1ddf1bef9869674abbb848ad39d0dbf1c3d8
Rami <alsaadi.rami24@gmail.com>
2023-08-22 00:30:29 +0200

    ui/ux edit delbutton margin increase to top/bottom

--- client/src/components/aside-left/ArrowToIcon.js
+++ client/src/components/aside-left/ArrowToIcon.js
@@ -10,7 +10,6 @@ import { Context } from "../Context";
 const ArrowToIcon = React.memo(({ toBottom, darkMode, shown }) => {
   const { state } = useContext(Context);
   function handleClick(e) {
-    e.preventDefault();
     window.scrollTo({
       top: toBottom ? window.document.body.clientHeight : 0,
     });
@@ -27,23 +26,22 @@ const ArrowToIcon = React.memo(({ toBottom, darkMode, shown }) => {
   }
 
   return (
-    <a
-      title={toBottom ? "Go To Bottom" : "Go To Top"}
+    <button
+      title={toBottom ? "Go To Bottom" : "Back To Top"}
       aria-label={toBottom ? "Scroll To Bottom" : "Scroll To Top"}
-      href={`${toBottom ? "#footer" : "#app"}`}
       onClick={handleClick}
-      className={`text-xl rounded-e-xl  bg-opacity-80 flex items-center justify-center fixed z-40  ${
+      className={`text-2xl rounded-e-2xl  bg-opacity-80 flex items-center justify-center fixed z-40  ${
         shown ? "-left-0.5" : "-left-8"
       } ${
         darkMode
           ? "bg-gray-900 text-gray-400 cursor-pointer hover:text-gray-200  focus:text-gray-200"
           : "bg-gray-400 text-amber-50 font-extrabold hover:bg-gray-500 focus:bg-gray-500"
-      } w-7 h-7 transition-all duration-700 ${
+      } w-8 h-8 transition-all duration-700 ${
         toBottom ? "-scale-y-100 bottom-5" : "top-5"
       } `}
     >
       <BiSolidArrowToTop />
-    </a>
+    </button>
   );
 });
 
--- client/src/components/main-div/item/DeleteButton.js
+++ client/src/components/main-div/item/DeleteButton.js
@@ -12,7 +12,7 @@ export default function DeleteButton({ darkMode, handleClick }) {
       aria-label="delete"
     >
       <MdDeleteForever
-        className={`ml-0.5 w-8 h-8 rounded-lg text-3xl text-center transition-transform duration-300 hover:scale-110 active:scale-125 hover:font-bold focus:font-bold inline-flex items-center justify-center leading-none focus:ring-2 focus:outline-none ${
+        className={`ml-1 w-8 h-8 rounded-lg text-3xl text-center transition-transform duration-300 hover:scale-110 active:scale-125 hover:font-bold focus:font-bold inline-flex items-center justify-center leading-none focus:ring-2 focus:outline-none ${
           darkMode
             ? " text-red-500 hover:text-red-400 focus:text-red-400"
             : " text-red-700 hover:text-red-800 focus:text-red-800"
