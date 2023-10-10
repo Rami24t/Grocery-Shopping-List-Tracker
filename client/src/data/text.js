a034bf83e7bd0f68771382d4342babb32fb4ff15
Rami <alsaadi.rami24@gmail.com>
2023-10-10 15:51:31 +0200

    Start implementing text data in multiple languages

---
+++ client/src/data/text.js
@@ -0,0 +1,29 @@
+import randomQuote from "./randomQuote";
+
+const LANGUAGES = ["English", "Deutsch", "العربيّة"];
+
+export const headerText = {
+  TITLES: [
+    "Grocery Shopping List",
+    "Einkaufsliste",
+    "قَاْئِمَةُ التَّسَوُّق الإلكِتْرُونِيَّة",
+  ],
+  SUBTITLES: [randomQuote[0], randomQuote[1], randomQuote[2]],
+  DARK_ON: [
+    "Theme is now dark",
+    "Dunkler Modus an",
+    "الوضع الدّاكن قيد التّشغيل",
+  ],
+  DARK_OFF: [
+    "Theme is now bright",
+    "Heller Modus an",
+    "الوضع السّاطع قيد التّشغيل",
+  ],
+  SOUND_ON: ["Sound is now on", "Ton an", "إلغاء كتم الصّوت"],
+  SOUND_OFF: ["Sound is now muted", "Ton aus", "كتم الصّوت"],
+};
+
+export const arrowToText = {
+  TO_TOP: ["Back To Top", "Zurück Nach Oben", "إلى فوق"],
+  TO_BOTTOM: ["Go To Bottom", "Nach Unten", "إلى تحت"],
+};
