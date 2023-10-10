8917617843a0846424334c95c89cee76afbd5e4f
Rami <alsaadi.rami24@gmail.com>
2023-10-10 16:14:05 +0200

    Design and Implement dynamic RTL LTR Designs

--- client/src/components/header/Hero.js
+++ client/src/components/header/Hero.js
@@ -5,12 +5,15 @@ function Hero({
   subtitle = "Let's do this!",
   darkMode,
   Icon,
+  rtlAlignment,
 }) {
   const getEmoji = () => (Math.random() * 100 > 50 ? "🧉" : "🚀");
 
   return (
     <div
-      className="ml-4 mr-4 md:mt-1 mb-6 md:mb-0 app-header-hero text-center lg:text-left select-none"
+      className={`ml-4 mr-4 md:mt-1 mb-6 md:mb-0 app-header-hero text-center select-none
+      ${rtlAlignment ? "lg:text-right" : "lg:text-left"}
+      `}
       style={{ minWidth: "59%" }}
     >
       <h1
@@ -18,15 +21,23 @@ function Hero({
           darkMode ? "text-white " : "text-red-700"
         } text-2xl font-bold sm:text-3xl`}
       >
-        {title} <Icon className="inline  " />
+        {rtlAlignment || title}{" "}
+        <Icon
+          className={`inline-block -translate-y-${
+            rtlAlignment ? "2" : "1 scale-110"
+          } -rotate-12`}
+        />{" "}
+        {rtlAlignment && title}
         {/* {list} */}
       </h1>
       <p
         className={`app-header-subtitle ${
           !darkMode ? "text-amber-600" : "text-[#989]"
-        } m-1.5 ml-1 p-1.5 mr-2.5 text-sm`}
+        }
+          ${rtlAlignment && "text-[0.98rem]"}
+         m-1.5 ml-1 p-1.5 mr-2.5 text-sm`}
       >
-        {subtitle} {getEmoji()}{" "}
+        {rtlAlignment || subtitle} {getEmoji()} {rtlAlignment && subtitle}
       </p>
     </div>
   );
