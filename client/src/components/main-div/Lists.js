72509ae737b866f2653efb1e264ed92d41f8ad33
Rami <alsaadi.rami24@gmail.com>
2023-10-21 00:32:45 +0200

    implement/add new languages/texts to lists.js

--- client/src/components/main-div/Lists.js
+++ client/src/components/main-div/Lists.js
@@ -1,6 +1,7 @@
 import { memo, useContext } from "react";
 import List from "./List";
 import { Context } from "../Context";
+import { listsText } from "../../data/text";
 
 function Lists({
   filteredNeeds,
@@ -14,20 +15,25 @@ function Lists({
   darkMode,
 }) {
   const { state, dispatch } = useContext(Context);
+  const { language } = state.settings;
+  const emptyListTitle = listsText.EMPTY_TITLE[language];
+  const emptyListsText = listsText.EMPTY_TEXT[language];
+  const needsTitle = listsText.NEEDS_TITLE[language];
+  const havesTitle = listsText.HAVES_TITLE[language];
 
   return !needs?.length && !haves.length ? (
     <article id="list" className="flex flex-col justify-around m-5 p-5">
-      <h3 className="text-lg">Empty list!</h3>
+      <h3 className="text-lg">{emptyListTitle}</h3>
       {/* <h3 className="text-lg">List {list} is Empty!</h3> */}
-      <a href="#new-item" className="text-sm m-4" title="Empty List! Add Items.">
-      Add some items to your list.
+      <a href="#new-item" className="text-sm m-4" title={emptyListTitle}>
+        {emptyListsText}
       </a>
     </article>
   ) : (
     <section id="list">
       {needs[0] && (
         <List
-          title="Need"
+          title={needsTitle}
           items={needs}
           filteredItems={filteredNeeds}
           showItems={state.showNeeds}
@@ -40,7 +46,7 @@ function Lists({
       )}
       {haves[0] && (
         <List
-          title="Have"
+          title={havesTitle}
           items={haves}
           filteredItems={filteredHaves}
           showItems={state.showHaves}
--- client/src/data/text.js
+++ client/src/data/text.js
@@ -9,16 +9,8 @@ export const headerText = {
     "قَاْئِمَةُ التَّسَوُّق الإلكِتْرُونِيَّة",
   ],
   SUBTITLES: [randomQuote[0], randomQuote[1], randomQuote[2]],
-  DARK_ON: [
-    "Theme is now dark",
-    "Dunkler Modus an",
-    "الوضع الدّاكن",
-  ],
-  DARK_OFF: [
-    "Theme is now bright",
-    "Heller Modus an",
-    "الوضع السّاطع",
-  ],
+  DARK_ON: ["Theme is now dark", "Dunkler Modus an", "الوضع الدّاكن"],
+  DARK_OFF: ["Theme is now bright", "Heller Modus an", "الوضع السّاطع"],
   SOUND_ON: ["Sound is now on", "Ton an", "إلغاء كتم الصّوت"],
   SOUND_OFF: ["Sound is now muted", "Ton aus", "كتم الصّوت"],
 };
@@ -31,7 +23,11 @@ export const arrowToText = {
 export const darkModeButtonText = {
   TITLE_DARK: ["To Bright Mode", "Zum Helleren Modus", "إلى الوضع السّاطع"],
   TITLE_LIGHT: ["To Dark Mode", "Zum Dunkleren Modus", "إلى الوضع الدّاكن"],
-  LABEL: ["Toggle dark/light modes", "Wechseln Sie zwischen hell und dunkel", "تبديل الوضع الدّاكن والسّاطع"],
+  LABEL: [
+    "Toggle dark/light modes",
+    "Wechseln Sie zwischen hell und dunkel",
+    "تبديل الوضع الدّاكن والسّاطع",
+  ],
 };
 
 export const soundButtonText = {
@@ -41,6 +37,25 @@ export const soundButtonText = {
 };
 
 export const listText = {
-  TITLE: ["Click to show/hide items", "Klicken Sie, um Elemente anzuzeigen/auszublenden", "انقر لإظهار/إخفاء العناصر"],
-  LABEL: ["Show/Hide items", "Elemente anzeigen/ausblenden", "إظهار/إخفاء العناصر"],
+  TITLE: [
+    "Click to show/hide items",
+    "Klicken Sie, um Elemente anzuzeigen/auszublenden",
+    "انقر لإظهار/إخفاء العناصر",
+  ],
+  LABEL: [
+    "Show/Hide items",
+    "Elemente anzeigen/ausblenden",
+    "إظهار/إخفاء العناصر",
+  ],
+};
+
+export const listsText = {
+  EMPTY_TITLE: ["Empty list!", "Leere Liste!", "قَاْئِمَةُ فَارِغَة!"],
+  EMPTY_TEXT: [
+    "Add some items to your list.",
+    "Fügen Sie einige Elemente zu Ihrer Liste hinzu.",
+    "أضف بعض العناصر إلى قائمتك.",
+  ],
+  NEEDS_TITLE: ["Need", "Brauche", "أحتاج"],
+  HAVES_TITLE: ["Have", "Habe", "لدي"],
 };
