563de0d0deb65254adc24dd448536508e2e0b97b
Rami <alsaadi.rami24@gmail.com>
2023-10-24 20:35:00 +0200

    implement rtlAlignment for info modal position

--- client/src/components/infoModal/InfoModal.js
+++ client/src/components/infoModal/InfoModal.js
@@ -1,4 +1,5 @@
-import { useEffect } from "react";
+import { useEffect, useContext } from "react";
+import { Context } from "../Context";
 
 function InfoModal({ info = "", setInfo, darkMode = true }) {
   const close = () => setInfo("");
@@ -13,13 +14,15 @@ function InfoModal({ info = "", setInfo, darkMode = true }) {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [info]);
 
+  const rtlAlignment = useContext(Context).state.settings.language === 2;
+
   if (!info) return null;
   return (
     <div
       onClick={close}
-      className={`z-50 p-1 cursor-pointer opacity-80 rounded-sm fixed left-7 bottom-11 ${
-        darkMode ? " bg-black text-white" : "bg-white text-black"
-      } modal`}
+      className={`z-50 p-1 cursor-pointer opacity-80 rounded-sm fixed bottom-11 ${
+        rtlAlignment ? "right-7 text-right" : "left-7"
+      } ${darkMode ? " bg-black text-white" : "bg-white text-black"} modal`}
     >
       <div>{info}</div>
     </div>
