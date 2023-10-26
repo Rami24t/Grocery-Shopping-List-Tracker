cde86b8657b9b64de93f69a3fe0f73f2d26d9475
Rami <alsaadi.rami24@gmail.com>
2023-10-27 00:17:48 +0200

    develop ui/ux of language button of small screens

--- client/src/components/header/LanguageButton.js
+++ client/src/components/header/LanguageButton.js
@@ -4,7 +4,11 @@ const LanguageButton = ({ text, handleClick, darkMode }) => {
   return (
     <button
       onClick={handleClick || (() => {})}
-      className="absolute top-2 left-2 z-50 sm:hidden"
+      className={`absolute rounded-xl top-2 left-2 z-50 sm:hidden border-2 ${
+        !darkMode
+          ? `bg-orange-100 border-gray-200 focus:ring-gray-300 focus:border-gray-300 hover:border-gray-300`
+          : `bg-gray-700 border-gray-700  focus:ring-gray-500 focus:border-gray-600 hover:border-gray-600`
+      }`}
     >
       <LanguageBadge text={text} darkMode={darkMode} />
     </button>
