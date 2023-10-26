0cc9efcb118d375a137064e226583c493d9a0422
Rami <alsaadi.rami24@gmail.com>
2023-10-26 05:18:20 +0200

    implement/add new languages/texts in TopFooter

--- client/src/components/footer/TopFooter.js
+++ client/src/components/footer/TopFooter.js
@@ -1,22 +1,36 @@
-import { memo } from "react";
+import { memo, useContext } from "react";
 import Company from "./Company";
 import FooterLists from "./FooterLists";
 import { navLinkClickSFXAudio } from "../../assets/sfx";
 import logoSvg from "../../assets/logo/logo.svg";
+import { topFooterText } from "../../data/text";
+import { Context } from "../Context";
 
-function TopFooter( {darkMode} ) {
-  return <div className={`rounded-sm md:pr-8 pt-6 items-center footer-top flex flex-col md:flex-row flex-wrap gap-2.5 justify-between
+function TopFooter({ darkMode }) {
+  const { language } = useContext(Context).state.settings;
+  const company = {
+    name: topFooterText.COMPANY_NAME[language],
+    subtext: topFooterText.SUBTEXT[language],
+  };
+
+  return (
+    <div
+      className={`rounded-sm md:pr-8 pt-6 items-center footer-top flex flex-col md:flex-row flex-wrap gap-2.5 justify-between
   ${darkMode ? "" : "bg-gradient-to-l to-yellow-200"}
-  `}>
-    <Company
-      logo={logoSvg}
-      name="Rami Al-Saadi"
-      link="https://www.linkedin.com/in/rami-al-saadi-16a14223a/" 
-      sfx={navLinkClickSFXAudio}
-      darkMode={darkMode} 
+  `}
+    >
+      <Company
+        logo={logoSvg}
+        name={company.name}
+        subtext={company.subtext}
+        link="https://www.linkedin.com/in/rami-al-saadi-16a14223a/"
+        sfx={navLinkClickSFXAudio}
+        darkMode={darkMode}
+        rtl={language === 2}
       />
-    <FooterLists  darkMode={darkMode} />
-  </div>;
+      <FooterLists darkMode={darkMode} />
+    </div>
+  );
 }
 
 export default memo(TopFooter);
