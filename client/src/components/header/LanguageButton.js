006a905cf0432937a3c959e08ddb2849cc5a0a9d
Rami <alsaadi.rami24@gmail.com>
2023-10-30 11:45:30 +0100

    edit language label/btn responsive styles

--- client/src/components/header/LanguageButton.js
+++ client/src/components/header/LanguageButton.js
@@ -8,7 +8,9 @@ const LanguageButton = ({ text, handleClick, darkMode }) => {
     <button
       onClick={handleClick || (() => {})}
       className={`absolute rounded-xl z-50 sm:hidden border-2 ${
-        en ? "top-0 left-0 border-1" : "top-1 left-0.5"
+        en && window.innerWidth < 403
+          ? "top-0 left-0 border-1"
+          : "top-1 left-0.5"
       } ${
         !darkMode
           ? `bg-orange-100 border-gray-200 focus:ring-gray-300 focus:border-gray-300 hover:border-gray-300`
