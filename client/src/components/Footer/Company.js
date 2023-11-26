a7043f2f3633bcc45695f6d2c46e631c31b0f0fe
Rami <alsaadi.rami24@gmail.com>
2023-11-26 13:10:56 +0100

    UX - Accessibility edit: arialabel in Company.js

--- client/src/components/Footer/Company.js
+++ client/src/components/Footer/Company.js
@@ -20,7 +20,7 @@ function Company({
     <div className="mb-6 md:mb-0">
       <a
         href={link}
-        aria-label={name + " - link"}
+        aria-label={`${name} ${subtext} - link`}
         onClick={handleClick}
         target="_blank"
         rel="noopener noreferrer"
