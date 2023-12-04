39e73f2dc21e1da1cc3ad8aeadc59f6d5083bfff
Rami <alsaadi.rami24@gmail.com>
2023-12-04 20:20:20 +0100

    ui edit: - items text position in rtl alignment

--- client/src/components/Main/Item/ItemNameInput.js
+++ client/src/components/Main/Item/ItemNameInput.js
@@ -92,6 +92,8 @@ function ItemNameInput({ item, updateItem, darkMode }) {
       onBlur={handleBlur}
       onKeyDown={handleKeyDown}
       className={`text-md w-56 sm:w-52 md:w-80 lg:min-w-[59%] p-2.5 m-2 inline-block leading-none rounded-lg bg-no-repeat bg-center bg-cover outline-[#0a0adc99] border focus:border-blue-500 focus:ring-blue-500 filter ${
+        rtlAlignment ? "text-right" : "text-left"
+      } ${
         darkMode
           ? "text-gray-50 contrast-[111%] hover:contrast-100 focus:contrast-100 bg-blend-darken disabled:bg-blend-multiply border-gray-700 placeholder-gray-400"
           : "text-white contrast-[105%] hover:contrast-125 focus:contrast-125 bg-blend-multiply disabled:bg-blend-darken border-gray-300 placeholder-gray-500"
