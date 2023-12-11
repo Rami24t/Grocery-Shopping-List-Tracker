80f362560ab7c4a81219a26b9beca5e62cf6b351
Rami <alsaadi.rami24@gmail.com>
2023-12-11 13:34:08 +0100

    UI/UX dev. in AddItems sect. +clean&format code

--- client/src/components/Main/AddItems.js
+++ client/src/components/Main/AddItems.js
@@ -23,7 +23,6 @@ function AddItems({ handleAdd, showAddItem, darkMode }) {
   const buttonTitle = addItemsText["BUTTON_TITLE"][language];
 
   const handleClick = (e) => {
-    // e.preventDefault();
     e.stopPropagation();
     e.currentTarget.previousSibling.focus();
   };
@@ -62,8 +61,10 @@ function AddItems({ handleAdd, showAddItem, darkMode }) {
       className={`overflow-hidden transition-all duration-500 ${
         !showAddItem
           ? "w-0 h-0 p-0 border-0 scale-0 opacity-50"
-          : `w-80 pt-3 pb-5 shadow-sm mb-5 ${rtlAlignment? "pl-3 " :"pr-3 "}`
-      } mt-4  mx-auto add-item border rounded-lg ${
+          : `w-80 pt-3 pb-5 shadow-sm mb-5 ${
+              rtlAlignment ? "pl-1 pr-3" : "pr-1 pl-3"
+            }`
+      } mt-4 max-w-max mx-auto add-item border rounded-lg ${
         darkMode
           ? "bg-gray-950 border-gray-800 text-white"
           : "bg-slate-50 bg-opacity-40 border-slate-300 text-gray-600"
@@ -77,8 +78,11 @@ function AddItems({ handleAdd, showAddItem, darkMode }) {
       >
         <h3 className="text-center text-lg">{title}</h3>
       </label>
-      {/* <div className="flex justify-center items-center gap-0"> */}
-      <div className={`flex justify-center items-center gap-0 ${rtlAlignment && "flex-row-reverse"}`} >
+      <div
+        className={`flex justify-center items-center gap-0 ${
+          rtlAlignment && "flex-row-reverse"
+        }`}
+      >
         <input
           onChange={handleChange}
           value={value}
@@ -89,11 +93,13 @@ function AddItems({ handleAdd, showAddItem, darkMode }) {
           type="text"
           id="new-item"
           autoComplete="on"
-          className={` ${rtlAlignment && "text-right"} ${
-            darkMode
-              ? "focus:border-blue-500 focus:ring-blue-500 placeholder-gray-400 text-white border-gray-600 bg-gray-700 bg-opacity-70 "
-              : "focus:ring-blue-300  focus:border-blue-300 placeholder-gray-400 bg-gray-50 bg-opacity-80 border-gray-300 text-gray-800"
-          } inline-block p-4 border rounded-lg sm:text-md outline-[#0a0adc99]`}
+          className={` ${rtlAlignment && "text-right"}
+          mx-0 transition-transform scale-95 focus:scale-100 w-full
+           ${
+             darkMode
+               ? "focus:border-blue-500 focus:ring-blue-500 placeholder-gray-400 text-white border-gray-600 bg-gray-700 bg-opacity-70 "
+               : "focus:ring-blue-300  focus:border-blue-300 placeholder-gray-400 bg-gray-50 bg-opacity-80 border-gray-300 text-gray-800"
+           } inline-block p-4 border rounded-lg sm:text-md outline-[#0a0adc99]`}
           onKeyDown={handleKeyDown}
         />
         <button
